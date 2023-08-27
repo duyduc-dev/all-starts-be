8b565a52afac6be97a87a94802d4376f64851779
duyduc-dev <dangduyducdangduyduc@gmail.com>
2023-08-27 10:34:43 +0700

    hello

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,4 +10,16 @@ export default function routerConfig(app) {
   rootRouter.use(apiPath.auth, authRouter);
 
   app.use(apiPath.baseUrl + apiPath.v1, rootRouter);
+  app.get(apiPath.index, (_, res) => {
+    res.json({
+      name: 'All stars',
+      source_name: 'all-star-be',
+      message: 'Welcome to "All Star Backend"',
+      now: new Date(),
+      instruction:
+        "1. Clone source code from 'https://github.com/duyduc-dev/all-starts-be/tree/production'\n" +
+        '2. Create file ".env". then add variable PORT=<your-port>. Example: PORT=3001\n' +
+        '3. Then run with develop by "npm run dev"',
+    });
+  });
 }
