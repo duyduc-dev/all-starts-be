26b6cf2a14045375c7d56f656596ea66d5503b76
tluuchau1 <tluuchau1@gmail.com>
2023-09-20 15:02:04 +0700

    title remove

--- src/validations/postValidation.js
+++ src/validations/postValidation.js
@@ -1,7 +1,6 @@
 import * as Yup from 'yup';
 
 const PostValidationSchema = Yup.object().shape({
-  title: Yup.string().required(),
   content: Yup.string().optional(),
   backgroundColor: Yup.string().optional(),
   image: Yup.string().optional(),
